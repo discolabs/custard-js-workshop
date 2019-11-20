8afb81140d9a7db939c5afb39840b44c9c360c65
Gavin Ballard <gavin@gavinballard.com>
2019-11-20 13:43:30 +1100

    Refactor console.log to a Custard.js module.

--- javascript/checkout/index.js
+++ javascript/checkout/index.js
@@ -1,1 +1,25 @@
-console.log('checkout.js loaded.');
+import { Custard, CustardModule, STEPS_ALL } from "@discolabs/custard-js";
+
+class LogToConsoleModule extends CustardModule {
+
+  id() {
+    return 'log-to-console';
+  }
+
+  steps() {
+    return STEPS_ALL;
+  }
+
+  selector() {
+    return 'body';
+  }
+
+  setup() {
+    console.log('checkout.js loaded.');
+  }
+
+}
+
+window.custard = new Custard([
+  LogToConsoleModule
+]);
